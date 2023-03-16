c1227e7a732fc7a0273e746fbad04c6e463e7ada
khussa1n <husain.khudaybergenov@gmail.com>
2023-03-16 19:05:00 +0600

    Update holiday api key

--- pkg/telegram/holiday.go
+++ pkg/telegram/holiday.go
@@ -2,10 +2,11 @@ package telegram
 
 import (
 	"github.com/joshtronic/go-holidayapi"
+	"github.com/spf13/viper"
 )
 
 func getHolidays(country, month, day string) (holidayapi.Respone, error) {
-	hapi := holidayapi.NewV1("baa0a205-4931-4e44-95c2-22ddde2ea585")
+	hapi := holidayapi.NewV1(viper.GetString("db.host"))
 
 	return hapi.Holidays(map[string]string{
 		"country":  country,
